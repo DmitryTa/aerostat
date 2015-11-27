40990c5e1e2e3e2fb0a507fc0b1c7b8997aab35c
DmitryTa <tskuadm@gmail.com>
2015-11-27 20:28:37 +0300

    last steps

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 		autoplayTimeout: 4500,
 		nav: true,
 		dotsEach: true,
-		
+		navText: [class="slider-arrow"],
 	});
 
 	$("#countdown").countdown({
@@ -24,4 +24,20 @@ $('.smoothScroll').click(function(event) {
     $('html,body').animate({
       scrollTop: top
     }, 2000);
+
     });
+
+$('#scene').parallax({
+  calibrateX: true,
+  calibrateY: true,
+  invertX: false,
+  invertY: true,
+  limitX: false,
+  limitY: 10,
+  scalarX: 15,
+  scalarY: 8,
+  frictionX: 0.2,
+  frictionY: 0.8,
+  originX: 0.0,
+  originY: 1.0
+});
